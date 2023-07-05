58ff1af32ce4e5699f5759bb75f0e6925f7ac0e1
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2023-07-05 17:18:57 +0700

    :money_with_wings: default main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
