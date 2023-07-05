fd0e0f1eee2b03786ce697b8666397b423844e81
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2023-07-06 00:22:05 +0700

    :money_with_wings: connect db

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,17 @@ mod config;
 mod database;
 
 use config::config::CONFIG;
-use database::DbPool;
+use database::database::db_connection;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let port: u16 = CONFIG.port.parse().expect("Failed to parse port number");
-    let database_url = &CONFIG.database_url;
 
-    let pool: DbPool = db_connection(database_url);
+    let connection = db_connection(&CONFIG.database_url);
 
     HttpServer::new(|| {
         App::new()
-            .data(pool.clone())
+            .app_data(web::Data::new(connection.clone()))
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
