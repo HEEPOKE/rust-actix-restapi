7600f10b729712efae2b3ef39d6fda32f51eba05
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2023-07-09 16:01:08 +0700

    :money_with_wings: update schema

--- src/schema/schema.rs
+++ src/schema/schema.rs
@@ -1,11 +1,5 @@
 // @generated automatically by Diesel CLI.
 
-pub mod sql_types {
-    #[derive(diesel::query_builder::QueryId, diesel::sql_types::SqlType)]
-    #[diesel(postgres_type(name = "user_role"))]
-    pub struct UserRole;
-}
-
 diesel::table! {
     shops (id) {
         id -> Int4,
@@ -20,7 +14,6 @@ diesel::table! {
 
 diesel::table! {
     use diesel::sql_types::*;
-    use super::sql_types::UserRole;
 
     users (id) {
         id -> Int4,
@@ -28,7 +21,6 @@ diesel::table! {
         email -> Varchar,
         password -> Nullable<Varchar>,
         tel -> Nullable<Varchar>,
-        role -> UserRole,
         created_at -> Timestamp,
         updated_at -> Timestamp,
     }
@@ -36,7 +28,4 @@ diesel::table! {
 
 diesel::joinable!(shops -> users (user_id));
 
-diesel::allow_tables_to_appear_in_same_query!(
-    shops,
-    users,
-);
+diesel::allow_tables_to_appear_in_same_query!(shops, users,);
