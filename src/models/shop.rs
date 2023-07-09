50eec1e36f7ad3d5dba03140d1693d60b30d8ca7
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2023-07-09 15:58:34 +0700

    :money_with_wings: add serde

--- src/models/shop.rs
+++ src/models/shop.rs
@@ -1,12 +1,14 @@
 use crate::models::user::User;
 use crate::schema::schema::shops;
-use chrono::NaiveDateTime;
 use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, PartialEq, Queryable, Selectable, Associations, Identifiable)]
-#[diesel(table_name = shops)]
-#[diesel(primary_key(id))]
-#[diesel(belongs_to(User))]
+#[derive(
+    Debug, PartialEq, Queryable, Selectable, Associations, Identifiable, Serialize, Deserialize,
+)]
+#[table_name = "shops"]
+#[primary_key(id)]
+#[belongs_to(User)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Shop {
     pub id: i32,
@@ -14,11 +16,9 @@ pub struct Shop {
     pub address: String,
     pub telephone: Option<String>,
     pub user_id: i32,
-    pub created_at: NaiveDateTime,
-    pub updated_at: NaiveDateTime,
 }
 
-#[derive(Debug, Insertable)]
+#[derive(Debug, Insertable, Serialize, Deserialize)]
 #[table_name = "shops"]
 pub struct NewShop<'a> {
     pub name: &'a str,
--- src/models/user.rs
+++ src/models/user.rs
@@ -1,17 +1,8 @@
-use chrono::NaiveDateTime;
-use diesel::prelude::*;
-
 use crate::schema::schema::users;
-use crate::schema::schema::users::dsl::*;
+use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, PartialEq, Eq, DbEnum, AsExpression)]
-#[DieselType = "UserRole"]
-pub enum UserRole {
-    Admin,
-    User,
-}
-
-#[derive(Debug, PartialEq, Queryable, Identifiable)]
+#[derive(Debug, PartialEq, Eq, Queryable, Identifiable, Serialize, Deserialize)]
 #[table_name = "users"]
 #[primary_key(id)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
@@ -21,17 +12,13 @@ pub struct User {
     pub email: String,
     pub password: Option<String>,
     pub tel: Option<String>,
-    pub role: UserRole,
-    pub created_at: NaiveDateTime,
-    pub updated_at: NaiveDateTime,
 }
 
-#[derive(Debug, PartialEq, Insertable)]
+#[derive(Debug, PartialEq, Insertable, Serialize, Deserialize)]
 #[table_name = "users"]
 pub struct NewUser<'a> {
     pub username: &'a str,
     pub email: &'a str,
     pub password: Option<&'a str>,
     pub tel: Option<&'a str>,
-    pub role: UserRole,
 }
