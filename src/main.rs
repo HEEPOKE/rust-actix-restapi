4d62b00dd4bb2430d172e531a6737c124ea992a3
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2023-07-22 01:56:04 +0700

    :money_with_wings: change prefix

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .app_data(web::Data::new(connection.clone()))
-            .service(scope("/api").service(routes::user_route::user_routes()))
+            .service(scope("/apis").service(routes::user_route::user_routes()))
     })
     .bind((CONFIG.host, port))?
     .run()
