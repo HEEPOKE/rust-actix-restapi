1b9ddd2465c1f270944ba052cfa3798ce2df96af
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2023-08-04 11:15:18 +0700

    :money_with_wings: refactor code

--- src/controllers/user_controller.rs
+++ src/controllers/user_controller.rs
@@ -1,18 +1,18 @@
-use crate::models::user::{NewUser, User};
+use crate::models::user::{NewUser};
 use crate::services::user_services::UserService;
-use actix_web::{web, HttpResponse, Responder};
+use actix_web::{web, HttpResponse};
 
-pub async fn get_all_users(user_service: web::Data<UserService>) -> impl Responder {
+pub async fn get_all_users(user_service: web::Data<UserService<'_>>) -> Result<HttpResponse, HttpResponse> {
     user_service
         .get_all_users()
         .map(|users| HttpResponse::Ok().json(users))
-        .map_err(|_| HttpResponse::InternalServerError().finish())
+        .map_err(|err| HttpResponse::InternalServerError().json(format!("Error: {:?}", err)))
 }
 
 pub async fn get_user_by_id(
-    user_service: web::Data<UserService>,
+    user_service: web::Data<UserService<'_>>,
     user_id: web::Path<i32>,
-) -> impl Responder {
+) -> Result<HttpResponse, HttpResponse> {
     let user_id = user_id.into_inner();
     user_service
         .get_user_by_id(user_id)
@@ -26,10 +26,10 @@ pub async fn get_user_by_id(
         .map_err(|_| HttpResponse::InternalServerError().finish())
 }
 
-pub async fn create_user(
-    user_service: web::Data<UserService>,
+pub async fn create_user<CreateUserRequest>(
+    user_service: web::Data<UserService<'_>>,
     new_user: web::Json<CreateUserRequest>,
-) -> impl Responder {
+) -> Result<HttpResponse, HttpResponse> {
     let new_user = NewUser {
         username: &new_user.username,
         email: &new_user.email,
@@ -42,11 +42,11 @@ pub async fn create_user(
         .map_err(|_| HttpResponse::InternalServerError().finish())
 }
 
-pub async fn update_user(
-    user_service: web::Data<UserService>,
+pub async fn update_user<UpdateUserRequest>(
+    user_service: web::Data<UserService<'_>>,
     user_id: web::Path<i32>,
     updated_user: web::Json<UpdateUserRequest>,
-) -> impl Responder {
+) -> Result<HttpResponse, HttpResponse> {
     let user_id = user_id.into_inner();
     let updated_user = NewUser {
         username: &updated_user.username,
@@ -61,9 +61,9 @@ pub async fn update_user(
 }
 
 pub async fn delete_user(
-    user_service: web::Data<UserService>,
+    user_service: web::Data<UserService<'_>>,
     user_id: web::Path<i32>,
-) -> impl Responder {
+) -> Result<HttpResponse, HttpResponse> {
     let user_id = user_id.into_inner();
     user_service
         .delete_user(user_id)
