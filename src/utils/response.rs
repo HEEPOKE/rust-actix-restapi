a88b0ba0760d53bcff048c08f2b40ed7854a87df
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2023-11-17 14:47:59 +0700

    update response error :money_with_wings:

--- src/utils/response.rs
+++ src/utils/response.rs
@@ -1,4 +1,4 @@
-use actix_web::{error::ResponseError, HttpResponse};
+use actix_web::{error::ResponseError, HttpResponse, http::StatusCode};
 use diesel::result::Error as DieselError;
 use derive_more::{Display, Error};
 
@@ -8,10 +8,42 @@ pub enum CustomError {
     DieselError(DieselError),
 }
 
+#[derive(Debug, Display, Error)]
+#[allow(dead_code)]
+pub enum ResError {
+    #[display(fmt = "internal error")]
+    InternalError,
+
+    #[display(fmt = "bad request")]
+    BadClientData,
+}
+
 impl ResponseError for CustomError {
     fn error_response(&self) -> HttpResponse {
         match self {
             CustomError::DieselError(_) => HttpResponse::InternalServerError().body("Internal Server Error"),
+        }
+    }
+
+    fn status_code(&self) -> StatusCode {
+        match self {
+            CustomError::DieselError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
+impl ResponseError for ResError {
+    fn error_response(&self) -> HttpResponse {
+        match self {
+            ResError::InternalError => HttpResponse::InternalServerError().body("Internal Server Error"),
+            ResError::BadClientData => HttpResponse::BadRequest().body("Bad Request"),
+        }
+    }
+
+    fn status_code(&self) -> StatusCode {
+        match self {
+            ResError::InternalError => StatusCode::INTERNAL_SERVER_ERROR,
+            ResError::BadClientData => StatusCode::BAD_REQUEST,
         }
     }
 }
